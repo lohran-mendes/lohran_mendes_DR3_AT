8cad90913854c952ba9b7ea6ac8349a5eaceb380
Lohran Mendes <lohranfm@gmail.com>
2025-04-01 21:19:36 -0300

    solved question 16

--- DR3.AT-16/script.js
+++ DR3.AT-16/script.js
@@ -9,3 +9,51 @@
 // Transforme o endereço em letras minúsculas e verifique se existe a palavra “rua”.
 // Link: https://codesandbox.io/p/sandbox/ydhjz8
 // Assim que terminar, salve seu trabalho em PDF nomeando o arquivo conforme a regra “nome_sobrenome_DR3_AT.PDF” e poste como resposta a este AT.
+
+const nome = document.querySelector("#nome");
+const rh = document.querySelector("#rh");
+const seguranca = document.querySelector("#seguranca");
+const cpf = document.querySelector("#cpf");
+const data = document.querySelector("#data");
+const endereco = document.querySelector("#endereco");
+const button = document.querySelector("#btnCadastrar");
+
+function validar() {
+  event.preventDefault();
+  if (
+    validaNome() &&
+    validaCheckbox() &&
+    validaCpf() &&
+    validaData() &&
+    validaEndereco()
+  ) {
+    alert("O formulário está válido!");
+  } else alert("O formulário não está válido!");
+}
+
+function validaNome() {
+  const tamanhoNome = nome.value.trim().length;
+  if (tamanhoNome > 3 && tamanhoNome < 10) return true;
+  return false;
+}
+
+function validaCheckbox() {
+  if (rh.checked || seguranca.checked) return true;
+  return false;
+}
+
+function validaCpf() {
+  const valorCpf = cpf.value.trim().replace(/[.-]/g, "");
+  if (valorCpf.length === 11) return true;
+  return false;
+}
+
+function validaData() {
+  if (data.value.includes("/")) return true;
+  return false;
+}
+
+function validaEndereco() {
+  if (endereco.value.trim().toLowerCase().includes("rua")) return true;
+  return false;
+}
